2cd3f033e5dcc5876c54ea23df3c489d8638bce4
Phan Hai Trieu <trieuphan3701@gmail.com>
2022-10-03 00:18:49 +0700

    them hinh

--- js/myJs.js
+++ js/myJs.js
@@ -35,9 +35,9 @@ $(document).ready(function () {
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,
-      imageUrl: "img/cuteCat.jpg",
+      imageUrl: "img/ThanhTruc.jpg",
       imageWidth: 300,
-      imageHeight: 300,
+      imageHeight: 360,
       background: '#fff url("img/iput-bg.jpg")',
       imageAlt: "Custom image",
     }).then(function () {
